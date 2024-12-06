5e45f0bdf39b1658e811eac131de8817308ae7ae
Varel05 <113572817+Varel05@users.noreply.github.com>
2024-12-06 17:24:16 +0700

    Delete pesan-menu directory

--- pesan-menu/besar/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = mysqli_connect("localhost", "root", "", "catering");
-if($host){
-    echo "Koneksi berhasil";
-} else{
-    echo "Koneksi gagal!" . mysqli_connect_error();
-    die();
-}
-?>
